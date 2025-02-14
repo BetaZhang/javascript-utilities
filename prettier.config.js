c2fb6803d8b8c4ea2935e27c0a3a61ad192d8b76
Zhang Zhe <102902468+BetaZhang@users.noreply.github.com>
2025-02-14 14:10:31 +0800

    Create prettier.config.js

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+const config = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  quoteProps: "as-needed",
+  trailingComma: "all",
+  bracketSpacing: true,
+  objectWrap: "preserve",
+  arrowParens: "always",
+};
+
+module.exports = config;
