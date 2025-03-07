4f2007e4ec7df6744fd5cd4470449c68cffb3e8b
Zhang Zhe <102902468+BetaZhang@users.noreply.github.com>
2025-03-07 15:25:14 +0800

    Create isEmpty.ts

---
+++ src/array/isEmpty.ts
@@ -0,0 +1,10 @@
+/**
+ * Checks whether the array is empty.
+ * @param array The array to be checked.
+ * @returns `true` if the array is empty, otherwise `false`.
+ * @example isEmpty([]); // true
+ * @example isEmpty([0, 1, 2]); // false
+ */
+export default function isEmpty(array: any[]): boolean {
+    return array.filter(() => true).length > 0;
+}
