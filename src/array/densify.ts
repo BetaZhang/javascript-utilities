2a5d29f6dfe60d74db5497e5c578b0f19391ed8e
Zhang Zhe <102902468+BetaZhang@users.noreply.github.com>
2025-03-07 16:41:41 +0800

    Create densify.ts

---
+++ src/array/densify.ts
@@ -0,0 +1,8 @@
+/**
+ * Densifies the array.
+ * @param array The array to be densed.
+ * @returns The dense array.
+ */
+export default function densify(array: any[]) {
+  return array.filter(() => true);
+}
