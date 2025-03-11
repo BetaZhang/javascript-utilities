7b7224a7a6fc422fdcad6466833c68110c4cbd0b
Zhang Zhe <102902468+BetaZhang@users.noreply.github.com>
2025-03-11 10:58:54 +0800

    [Change] Update sum.ts

--- src/math/sum.ts
+++ src/math/sum.ts
@@ -1,10 +1,9 @@
-import { NonEmptyArray } from '../types/array';
-
 /**
  * Calculates the sum of multiple numbers.
+ * @param numbers The numbers to sum.
  * @returns The sum of the numbers.
  * @example sum(0, 1, 2); // 3
  */
-export default function sum(number: number, ...others: NonEmptyArray<number>): number {
-  return others.reduce((previousValue, currentValue) => previousValue + currentValue, number);
+export default function sum(...numbers: number[]): number {
+  return numbers.reduce((previousValue, currentValue) => previousValue + currentValue);
 }
